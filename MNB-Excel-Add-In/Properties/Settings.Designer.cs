140bed95ba8e5b556d51d3d591cee93477086c45
Ferenc Sas <sasferi95@gmail.com>
2020-08-08 10:18:05 +0200

    Added web reference

--- MNB-Excel-Add-In/Properties/Settings.Designer.cs
+++ MNB-Excel-Add-In/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace MNB_Excel_Add_In.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.7.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -20,6 +20,16 @@ namespace MNB_Excel_Add_In.Properties {
         public static Settings Default {
             get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.WebServiceUrl)]
+        [global::System.Configuration.DefaultSettingValueAttribute("http://www.mnb.hu/arfolyamok.asmx")]
+        public string MNB_Excel_Add_In_hu_mnb_www_MNBArfolyamServiceSoapImpl {
+            get {
+                return ((string)(this["MNB_Excel_Add_In_hu_mnb_www_MNBArfolyamServiceSoapImpl"]));
             }
         }
     }
